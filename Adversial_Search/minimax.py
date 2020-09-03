0b2a01127b08815586cf2bb4fe3aaeb017f05e89
Tommy Ernsund <13759796+ern1@users.noreply.github.com>
2020-09-03 05:19:38 +0200

    fix some comments and syntax

--- Adversial_Search/minimax.py
+++ Adversial_Search/minimax.py
@@ -7,30 +7,34 @@ def utility(board):
         p2 = board[13] + board[8:13].count(0)
         return p1 - p2
 
+
 def terminal_test(board):
     return not any(board[:6]) or not any(board[7:13])
 
+
 def get_possible_moves(board, playerTurn):
-    if playerTurn == 1: return [i for i,x in enumerate(board[0:6]) if x]
-    else:               return [i for i,x in enumerate(board[7:13], start=7) if x]
-        
+    if playerTurn == 1: return [i for i, x in enumerate(board[0:6]) if x]
+    else:               return [i for i, x in enumerate(board[7:13], start=7) if x]
+
+
 def result(board, playerTurn, move):
     new_board = list(board)
     new_board[move] = 0
     
-    player_store = playerTurn * 7 - 1           # index for the players store
-    start, end = move + 1, move + board[move]   # range to distribute the pieces over
-    end += (board[move] + 1 + (move % 7)) // 14 # Add number of steps that would be in opponents store
+    player_store = playerTurn * 7 - 1            # index for the player's store
+    start, end = move + 1, move + board[move]    # range to distribute the pieces over
+    end += (board[move] + 1 + (move % 7)) // 14  # add number of steps that would be in opponents store
     
+    # distribute piecies
     for i in range(start, end + 1):
-        if not (i + 7) % 14 == player_store: # Skip opponents store
+        if not (i + 7) % 14 == player_store:  # Skip opponents store
             new_board[i % 14] += 1
         
-    end_index = end % 14 # index of where the last piece was dropped
+    end_index = end % 14  # index of where the last piece was dropped
     if end_index in range(player_store - 6, player_store):
-        # If it was dropped in an empty hole if there is 1 piece in it now
+        # If it was dropped in an empty hole there is 1 piece in it now
         if new_board[end_index] == 1:
-            # Captue last piece and any pieces in hole directly opposite
+            # Capture last piece and any pieces in hole directly opposite
             new_board[player_store] += new_board[12 - end_index] + 1
             new_board[end_index], new_board[12 - end_index] = 0, 0
     
@@ -38,6 +42,7 @@ def result(board, playerTurn, move):
     if not end_index == player_store:
         playerTurn ^= 0b11
     return new_board, playerTurn
+
 
 def minimax(board, playerTurn, d):
     if terminal_test(board) or not d:
@@ -53,10 +58,10 @@ def minimax(board, playerTurn, d):
             v = min(v, minimax(*result(board, playerTurn, move), d - 1))
     return v
 
+
 # Return an action (move)
 def minimax_decision(board, playerTurn, max_depth=3):
-    actions = [(minimax(*result(board, playerTurn, a), max_depth - 1), a) for a in get_possible_moves(board, playerTurn)]
-    return str(
-        max(actions, key=lambda x: x[0])[1] + 1 if playerTurn == 1 else
-        min(actions, key=lambda x: x[0])[1] - 6
-    )
+    actions = [(minimax(*result(board, playerTurn, a), max_depth - 1), a)
+               for a in get_possible_moves(board, playerTurn)]
+    return str(max(actions, key=lambda x: x[0])[1] + 1 if playerTurn == 1 else
+               min(actions, key=lambda x: x[0])[1] - 6)
